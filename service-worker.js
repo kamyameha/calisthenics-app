58d4094edbc99ea9aa7a7500075de9f551852be1
kamyameha <grascam@gmail.com>
2026-06-15 19:07:44 +0300

    Add new PWA app icon

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,24 @@
-const CACHE_NAME = 'calisthenics-pwa-v2';
-const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json'];
+const CACHE_NAME = 'calisthenics-pwa-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icons/icon-512.png',
+  './icons/icon-maskable-512.png'
+];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
 });
 
 self.addEventListener('fetch', event => {
